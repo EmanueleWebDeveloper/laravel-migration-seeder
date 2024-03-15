27115a958a3979a1510c64bf3e723f82324a9f32
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-03-15 20:00:56 +0100

    Seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,13 +5,29 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker; // Importa il generatore di dati falsi
+
+use App\Models\Train;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            Train::create([
+                'Azienda' => $faker->company,
+                'Stazione_partenza' => $faker->city,
+                'Stazione_arrivo' => $faker->city,
+                'Orario_partenza' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'Orario_arrivo' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'Codice_Treno' => $faker->unique()->ean8,
+                'Numero_Carrozze' => $faker->numberBetween($min = 1, $max = 10),
+                'In_orario' => $faker->boolean,
+                'Cancellato' => $faker->boolean,
+            ]);
+        }
     }
 }
