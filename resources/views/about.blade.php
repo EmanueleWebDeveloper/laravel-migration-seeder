4036e89f1b5c2f6acd1b110caaf18f7e1b70bb2c
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-03-15 16:04:38 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.basic')
+
+@section('title', 'Benvenuto | About')
+
+@section('content')
+    <h1>Pagina di about</h1>
+
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.basic')
+
+@section('title', 'Benvenuto | Home')
+
+@section('content')
+    <h1>Pagina di benvenuto</h1>
+
+    <ul>
+
+            {{-- {{ dd($loop) }} --}}
+
+            <h1>ciao mondo</h1>
+    </ul>
+@endsection
